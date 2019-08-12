b3b97dc0abeb218fb3f335dc17a6b3ff09e0c4c9
Neowyne <krisztina.skravan@gmail.com>
2019-08-12 13:27:02 +0200

    further adjustments to the index page

--- http-client-test/src/app/page/index/index.component.ts
+++ http-client-test/src/app/page/index/index.component.ts
@@ -7,13 +7,34 @@ import { UserService } from 'src/app/service/user.service';
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+  userSubscription: any;
+  userAmount: number = 0;
+  userActiveAmount: number = 0;
+  userInactiveAmount: number = 0;
+  balanceSum: number = 0;
+  favoriteApple: number = 0;
+  favoriteStrawberry: number = 0;
+  favoriteBanana: number = 0;
 
 
   constructor(
     private userService: UserService
-  ) { }
+  ) {
+    this.userSubscription = this.userService.getAll().subscribe(
+
+    )
+  }
 
   ngOnInit() {
+  }
+
+  getUserAmount() {
+
+
+  }
+
+  getBalance() {
+
   }
 
 
